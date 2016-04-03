dcc84840e7429b52200cb6ab802b9cf6212dfb7c
あまみや ゆうこ <1@1234.sh>
2016-04-03 18:27:33 +0800

    Created subject CodeComb.AspNet.Flow

---
+++ src/CodeComb.AspNet.Flow/ApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+
+namespace CodeComb.AspNet.Flow
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseCodeCombFlow(this IApplicationBuilder self)
+        {
+            
+        }
+    }
+}
---
+++ src/CodeComb.AspNet.Flow/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CodeComb.AspNet.Flow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodeComb.AspNet.Flow")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0f1b5a73-65cd-4d1b-aa10-65c73f9eeaed")]
